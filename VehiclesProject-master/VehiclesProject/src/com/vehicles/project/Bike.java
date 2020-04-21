9b44272ce870aacf57d981790dafafd4524dcd89
Anton Lage <hagane.link0gmail.com>
2020-04-21 12:57:26 +0200

    Third Commit. Phase 3 ended.

--- VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Bike.java
+++ VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Bike.java
@@ -1,9 +1,24 @@
 package com.vehicles.project;
 
+import java.util.List;
+
 public class Bike extends Vehicle {
 
 	public Bike(String plate, String brand, String color) {
 		super(plate, brand, color);
 	}
+	
+	public void addWheels(List<Wheel> wheels) throws Exception {
+		if (wheels.size() != 2)
+			throw new Exception();
+		this.wheels = wheels;
+	}
+
+	@Override
+	public String toString() {
+		return "Bike [plate=" + plate + ", brand=" + brand + ", color=" + color + ", wheels=" + wheels + "]";
+	}
+	
+	
 
 }
--- VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Main.java
+++ VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Main.java
@@ -8,19 +8,40 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		Car userCar = createCar(reader);
-		System.out.println(userCar);
+		Vehicle userVehicle = createVehicle(reader);
+		System.out.println(userVehicle);
 		reader.close();
 
 
 	}
 	
-	public static Car createCar(Scanner reader) {
-		String plate = createPlate(reader);
-		System.out.println("Please, insert the car's brand:");
-		String brand = reader.nextLine();
-		System.out.println("Please, insert the car's color:");
-		String color = reader.nextLine();
+	//Asks the user if he wants to create a bike or a car and proceeds with it.
+	public static Vehicle createVehicle(Scanner reader) {
+		while(true) {
+			System.out.println("What do you want o create, a bike or a car");
+			String vehicle = reader.nextLine();
+			if (vehicle.equalsIgnoreCase("bike") || vehicle.equalsIgnoreCase("car")) {
+				String plate = createPlate(reader);
+				System.out.println("Please, insert the brand:");
+				String brand = reader.nextLine();
+				System.out.println("Please, insert the color:");
+				String color = reader.nextLine();
+				if (vehicle.equalsIgnoreCase("car")) {
+					Car userCar = createCar(reader, plate, brand, color);
+					return userCar;
+				} else {
+					Bike userBike = createBike(reader, plate, brand, color);
+					return userBike;
+				}
+			} else {
+				System.out.println("Invalid keyword.");
+			}
+		}
+		
+	}
+	
+	//asks the user for the wheels and creates a car
+	public static Car createCar(Scanner reader, String plate, String brand, String color) {
 		Car userCar = new Car(plate, brand, color);
 		System.out.println("Now, we will add the front wheels.");
 		List<Wheel> frontWheels = createWheels(reader);
@@ -35,6 +56,7 @@ public class Main {
 		return userCar;
 	}
 	
+	//checks if the plate contains a valid amount of numbers and letters
 	public static boolean checkPlate(String plate) {
 		int amountOfNumbers = 0;
 		int amountOfLetters = 0;
@@ -52,41 +74,63 @@ public class Main {
 		}
 	}
 	
+	//creates a valid plate
 	public static String createPlate(Scanner reader) {
 		while(true) {
-			System.out.println("Please, insert the car plate number:");
+			System.out.println("Please, insert the plate number:");
 			String plate = reader.nextLine();
 			if (checkPlate(plate)) {
 				return plate;
 			} else {
 				System.out.println("The plate isn't valid. Please insert a plate with 4 numbers and two or three letters.");
 			}
-		}
-		
+		}	
 	}
 	
+	//creates a List of two equal wheels
 	public static List<Wheel> createWheels(Scanner reader) {
-		System.out.println("Insert the brand of the front wheels:");
+			Wheel wheel = createWheel(reader);
+			Wheel right = wheel;
+			Wheel left = wheel;
+			List<Wheel> wheels = new ArrayList<Wheel>();
+			wheels.add(right);
+			wheels.add(left);
+			return wheels;
+	}
+	
+	//creates a single wheel with the right parameters
+	public static Wheel createWheel(Scanner reader) {
+		System.out.println("Insert the brand of the wheel:");
 		String wheelBrand = reader.nextLine();
 		while(true) {
-			System.out.println("Insert the diammeter of the front wheels:");
+			System.out.println("Insert the diammeter of the wheel:");
 			double diameter = Double.parseDouble(reader.nextLine());
 			if (diameter < 4 && diameter > 0.4) {
 				Wheel wheel = new Wheel(wheelBrand, diameter);
-				Wheel right = wheel;
-				Wheel left = wheel;
-				List<Wheel> wheels = new ArrayList<Wheel>();
-				wheels.add(right);
-				wheels.add(left);
-				return wheels;
+				return wheel;
 			} else {
 				System.out.println("The diameter of the wheel isn't valid. Please insert a number lower than 4 and higher than 0.4");
-			}
-			
+			}	
 		}
-		
-
-
+	}
+	
+	//aks the user for the wheels and creates a bike
+	public static Bike createBike(Scanner reader, String plate, String brand, String color) {
+		Bike userBike = new Bike(plate, brand, color);
+		System.out.println("Now we will add the front wheel");
+		Wheel frontWheel = createWheel(reader);
+		System.out.println("Now we will add the back wheel");
+		Wheel backWheel = createWheel(reader);
+		List<Wheel> wheels = new ArrayList<Wheel>();
+		wheels.add(backWheel);
+		wheels.add(frontWheel);
+		try {
+			userBike.addWheels(wheels);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return userBike;
 	}
 	
 
